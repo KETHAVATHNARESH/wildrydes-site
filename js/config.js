f368d044109365b71c6bb36922dbc828b6b4ef66
KETHAVATHNARESH <kethavathnaresh053@gmail.com>
2025-07-30 12:36:49 +0530

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,7 +1,7 @@
 window._config = {
     cognito: {
-        userPoolId: 'ap-south-1_w0pcJWKo8', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '1c4m90gl2q09tv67d8uip5hjfa', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        userPoolId: 'ap-south-1_hwWsdqxKL', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '3v0ecokodbgti49li8qabgdf0t', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
         region: 'ap-south-1' // e.g. us-east-2
     },
     api: {
